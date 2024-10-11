38fb2c90fa1561dcac19fa51c1546888dcd810fc
Vladislav Parkhachev <vladparhachev777@gmail.com>
2024-10-12 02:32:22 +0300

    Add moveRobot

--- task_1.cpp
+++ task_1.cpp
@@ -7,7 +7,11 @@
 #include <stack>
 #include <vector>
 #include <tuple>
+
 using json = nlohmann::json;
+
+template <typename T, std::size_t Row, std::size_t Col>
+using Matrix = std::array<std::array<T, Col>, Row>;
 
 int gotPoints = 0;
 int distance_wall = 70;
@@ -46,74 +50,110 @@ SensorsData rotationSensors(SensorsData sensors_data);
 void printSensorsData(const SensorsData &sensors_data);
 int cellType(const SensorsData &rotation_sensor_data);
 
+void moveRobot(const SensorsData &sensots_data, int &row, int &col);
+
 // int cellType(std::vector<double> &vals);
 
-std::array<std::array<bool,16>,16> visited;
-
-template <typename T, std::size_t Row, std::size_t Col>
-using Matrix = std::array<std::array<T, Col>, Row>;
+std::array<std::array<bool, 16>, 16> visited;
 
 template <typename T, std::size_t Row, std::size_t Col>
 void sendMatrixMaze(const Matrix<T, Row, Col> &arr_matrix);
 
 template <typename T, std::size_t Row, std::size_t Col>
-int explore(int& x, int& y,SensorsData& current_sensors_data,const Matrix<T, Row, Col> &visited);
+int explore(int &x, int &y, SensorsData &current_sensors_data, const Matrix<T, Row, Col> &visited);
+
 int main()
 {
+    Matrix<int, 16, 16> arr_matrix{};
+    Matrix<bool, 16, 16> visited_matrix{};
+
     SensorsData current_sensors_data;
     SensorsData rotation_sensors_data;
-    int x = 0,y = 15;
-    Matrix<int, 16, 16> arr_matrix;
-    Matrix<int, 16, 16> transposed_arr_matrix;
-    Matrix<bool, 16, 16> visited_matrix;
 
-    for (auto& row : arr_matrix) {
-        std::fill(row.begin(), row.end(), -1);
-    }
+    int row = 15, col = 0;
+    int count = 1;
+    
+    // getSensorsData(current_sensors_data);
+    // 
 
-    while(gotPoints<256){
+    for(int i = 0; i < 100; i++)
+    {
         getSensorsData(current_sensors_data);
         rotation_sensors_data = rotationSensors(current_sensors_data);
-        arr_matrix[x][y] = cellType(rotation_sensors_data);
-        visited[x][y]=true;
-        if(explore(x,y,current_sensors_data,visited)==2) break;
-        // std::cout<<x<<" "<<y<<"   ";
+        arr_matrix.at(row).at(col) = cellType(rotation_sensors_data);
+        visited_matrix.at(row).at(col) = 1;
+        std::cout << "row: " << row << "  col: " << col << std::endl;
+        printSensorsData(current_sensors_data);
+        moveRobot(current_sensors_data, row, col);
     }
-    for (size_t i = 0; i < arr_matrix.size(); ++i) {
-        for (size_t j = 0; j < arr_matrix[i].size(); ++j) {
-            transposed_arr_matrix[j][i] = arr_matrix[i][j];
-        }
-    }
-    sendMatrixMaze(transposed_arr_matrix);
-    std::cout<<gotPoints<<std::endl;
-    while (!moveHistory.empty()) {
-        auto item = moveHistory.top(); // Получаем верхний элемент
-        std::cout << "Элемент: (" 
-                  << std::get<0>(item) << ", " 
-                  << std::get<1>(item) << ", " 
-                  << std::get<2>(item) << ")" <<"  ";
-        moveHistory.pop(); // Удаляем верхний элемент
-    }
-    std::cout<<std::endl;
+
+    sendMatrixMaze(arr_matrix);
+
+
+    // json json_array(arr_matrix);
+    // auto string_matrix_maze = to_string(json_array);
+    // std::cout << string_matrix_maze << std::endl;
+
+
+    // if(visited_matrix.at(row).at(col) == 0)
+    // {
+    //     moveRobot(current_sensors_data, visited_matrix, row, col);
+    // }
+
+    // visited_matrix.at(row).at(col) = 1;
+    // }
+
+    //     for (auto& row : arr_matrix) {
+    //         std::fill(row.begin(), row.end(), -1);
+    //     }
+
+    //     while(gotPoints<256){
+    //         getSensorsData(current_sensors_data);
+    //         rotation_sensors_data = rotationSensors(current_sensors_data);
+    //         arr_matrix[x][y] = cellType(rotation_sensors_data);
+    //         visited[x][y]=true;
+    //         if(explore(x,y,current_sensors_data,visited)==2) break;
+    //         // std::cout<<x<<" "<<y<<"   ";
+    //     }
+    //     for (size_t i = 0; i < arr_matrix.size(); ++i) {
+    //         for (size_t j = 0; j < arr_matrix[i].size(); ++j) {
+    //             transposed_arr_matrix[j][i] = arr_matrix[i][j];
+    //         }
+    //     }
+    //     sendMatrixMaze(transposed_arr_matrix);
+    //     std::cout<<gotPoints<<std::endl;
+    //     while (!moveHistory.empty()) {
+    //         auto item = moveHistory.top(); // Получаем верхний элемент
+    //         std::cout << "Элемент: ("
+    //                   << std::get<0>(item) << ", "
+    //                   << std::get<1>(item) << ", "
+    //                   << std::get<2>(item) << ")" <<"  ";
+    //         moveHistory.pop(); // Удаляем верхний элемент
+    //     }
+    //     std::cout<<std::endl;
 }
 template <typename T, std::size_t Row, std::size_t Col>
-bool isRightCellNotVis(int x,int y,const Matrix<T, Row, Col> &visited,const std::vector<std::pair<int, int>>& directions){
-    return !visited[x+directions[1].first][y+directions[1].second];
+bool isRightCellNotVis(int x, int y, const Matrix<T, Row, Col> &visited, const std::vector<std::pair<int, int>> &directions)
+{
+    return !visited[x + directions[1].first][y + directions[1].second];
 }
 template <typename T, std::size_t Row, std::size_t Col>
-bool isFrontCellNotVis(int x,int y,const Matrix<T, Row, Col> &visited,const std::vector<std::pair<int, int>>& directions){
-    return !visited[x+directions[0].first][y+directions[0].second];
+bool isFrontCellNotVis(int x, int y, const Matrix<T, Row, Col> &visited, const std::vector<std::pair<int, int>> &directions)
+{
+    return !visited[x + directions[0].first][y + directions[0].second];
 }
 template <typename T, std::size_t Row, std::size_t Col>
-bool isLeftCellNotVis(int x,int y,const Matrix<T, Row, Col> &visited,const std::vector<std::pair<int, int>>& directions){
-    return !visited[x+directions[3].first][y+directions[3].second];
+bool isLeftCellNotVis(int x, int y, const Matrix<T, Row, Col> &visited, const std::vector<std::pair<int, int>> &directions)
+{
+    return !visited[x + directions[3].first][y + directions[3].second];
 }
 template <typename T, std::size_t Row, std::size_t Col>
-int explore(int& x, int& y,SensorsData& current_sensors_data,const Matrix<T, Row, Col> &visited){
+int explore(int &x, int &y, SensorsData &current_sensors_data, const Matrix<T, Row, Col> &visited)
+{
     std::vector<std::pair<int, int>> directions;
-    std::cout<<current_sensors_data.rotation_yaw<<" \n";
+    std::cout << current_sensors_data.rotation_yaw << " \n";
     switch ((int)current_sensors_data.rotation_yaw)
-    {//вперёд,вправо,назад,влево
+    { // вперёд,вправо,назад,влево
     case 90:
         directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
         break;
@@ -133,34 +173,41 @@ int explore(int& x, int& y,SensorsData& current_sensors_data,const Matrix<T, Row
         break;
     }
     bool front = current_sensors_data.front_distance > distance_wall,
-    right = current_sensors_data.right_side_distance > distance_wall,
-    left = current_sensors_data.left_side_distance > distance_wall,
-    back = current_sensors_data.back_distance > distance_wall;
-    std::cout<<front<<" "<<right<<" "<<left<<" "<<back<<std::endl;
-    if(right && isRightCellNotVis(x,y,visited,directions)){//right
-        moveHistory.push(std::make_tuple('r',x,y));
+         right = current_sensors_data.right_side_distance > distance_wall,
+         left = current_sensors_data.left_side_distance > distance_wall,
+         back = current_sensors_data.back_distance > distance_wall;
+    std::cout << front << " " << right << " " << left << " " << back << std::endl;
+    if (right && isRightCellNotVis(x, y, visited, directions))
+    { // right
+        moveHistory.push(std::make_tuple('r', x, y));
         move_robot::Right();
         move_robot::Forward();
-        gotPoints+=1;
-        x+=directions[1].first;
-        y+=directions[1].second;
+        gotPoints += 1;
+        x += directions[1].first;
+        y += directions[1].second;
         return 0;
-    }else if(front && isFrontCellNotVis(x,y,visited,directions)){//front
-        moveHistory.push(std::make_tuple('f',x,y));
+    }
+    else if (front && isFrontCellNotVis(x, y, visited, directions))
+    { // front
+        moveHistory.push(std::make_tuple('f', x, y));
         move_robot::Forward();
-        gotPoints+=1;
-        x+=directions[0].first;
-        y+=directions[0].second;
+        gotPoints += 1;
+        x += directions[0].first;
+        y += directions[0].second;
         return 0;
-    }else if(left && isLeftCellNotVis(x,y,visited,directions)){//left
-        moveHistory.push(std::make_tuple('l',x,y));
+    }
+    else if (left && isLeftCellNotVis(x, y, visited, directions))
+    { // left
+        moveHistory.push(std::make_tuple('l', x, y));
         move_robot::Left();
         move_robot::Forward();
-        gotPoints+=1;
-        x+=directions[3].first;
-        y+=directions[3].second;
+        gotPoints += 1;
+        x += directions[3].first;
+        y += directions[3].second;
         return 0;
-    }else if(back){
+    }
+    else if (back)
+    {
         // move_robot::Backward();НЕ работает
         // auto item = moveHistory.top();
         // char dir= std::get<0>(item);
@@ -192,9 +239,91 @@ int explore(int& x, int& y,SensorsData& current_sensors_data,const Matrix<T, Row
         // }
         // std::cout<<"x: "<<x<<" y: "<<y<<std::endl;
         return 1;
-    }else return 1;
+    }
+    else
+        return 1;
+}
 
-} 
+void moveRobot(const SensorsData &sensots_data, int &row, int &col)
+{
+    if (sensots_data.right_side_distance > 70)
+    {
+        switch ((int)sensots_data.rotation_yaw)
+        {
+        case 90:
+            row++;
+            break;
+        case -180:
+            col--;
+            break;
+        // case 180:
+        //     col--;
+        //     break;
+        case -90:
+            row--;
+            break;
+        case 0:
+            col++;
+            break;
+        }
+
+        move_robot::Right();
+    }
+
+    else if (sensots_data.front_distance > 70)
+    {
+        switch ((int)sensots_data.rotation_yaw)
+        {
+        case 90:
+            col++;
+            break;
+        case -180:
+            row++;
+            break;
+        // case 180:
+        //     row++;
+        //     break;
+        case -90:
+            col--;
+            break;
+        case 0:
+            row--;
+            break;
+        }
+    }
+
+    else if (sensots_data.left_side_distance > 70)
+    {
+        switch ((int)sensots_data.rotation_yaw)
+        {
+        case 90:
+            row--;
+            break;
+        case -180:
+            col++;
+            break;
+        // case 180:
+        //     col++;
+        //     break;
+        case -90:
+            row++;
+            break;
+        case 0:
+            col--;
+            break;
+        }
+
+        move_robot::Left();
+    }
+    else if (sensots_data.back_distance > 70)
+    {
+        move_robot::Right();
+        move_robot::Right();
+    }
+
+    move_robot::Forward();
+}
+
 void getSensorsData(SensorsData &sensors_data)
 {
     auto r = cpr::Get(cpr::Url{api_interface::sensor_data});
@@ -211,7 +340,7 @@ void sendMatrixMaze(const Matrix<T, Row, Col> &arr_matrix)
 {
     json json_array(arr_matrix);
     auto string_matrix_maze = to_string(json_array);
-    std::cout<<string_matrix_maze<<std::endl;
+    std::cout << string_matrix_maze << std::endl;
     cpr::Response r = cpr::Post(cpr::Url{api_interface::matrix_send},
                                 cpr::Body{string_matrix_maze},
                                 cpr::Header{{"Content-Type", "application/json"}});
@@ -223,7 +352,7 @@ void move_robot::Forward()
     cpr::Response r = cpr::Post(cpr::Url{api_interface::forward},
                                 cpr::Body{""},
                                 cpr::Header{{"accept", "application/json"}});
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
 }
 
 void move_robot::Right()
@@ -240,13 +369,12 @@ void move_robot::Left()
                                 cpr::Header{{"accept", "application/json"}});
 }
 
-void move_robot::Backward()
-{
-    move_robot::Right();
-    move_robot::Right();
-    move_robot::Forward();
-}
-
+// void move_robot::Backward()
+// {
+//     move_robot::Right();
+//     move_robot::Right();
+//     move_robot::Forward();
+// }
 
 SensorsData rotationSensors(SensorsData sensors_data)
 {
@@ -367,6 +495,6 @@ int cellType(const SensorsData &rotation_sensor_data)
     {
         return 15;
     }
-    
+
     return -1;
 }
